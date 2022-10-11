61fec72df757236855f2dfc19eb65490feb94064
pf4 <74263128+elydre@users.noreply.github.com>
2022-10-11 13:51:44 +0200

    fix float int

--- zapps/3drenderer.c
+++ zapps/3drenderer.c
@@ -1,23 +1,23 @@
 #include "syscall.h"
 
 typedef struct Vertex2D_t {
-    int x;
-    int y;
+    float x;
+    float y;
 } Vertex2D_t;
 
 typedef struct Vertex3D_t {
-    int x;
-    int y;
-    int z;
+    float x;
+    float y;
+    float z;
 } Vertex3D_t;
 
 typedef struct Cube_t {
-    int d;
+    float d;
     Vertex3D_t vertices[8];
     int indices[36];
 } Cube_t;
 
-Cube_t Cube_t_init(Vertex3D_t position, int size);
+Cube_t Cube_t_init(Vertex3D_t position, float size);
 Vertex2D_t Vertex2D_t_project(Vertex3D_t vector);
 float crossProduct(Vertex2D_t v0, Vertex2D_t v1, Vertex2D_t v2);
 
@@ -33,10 +33,10 @@ int main(int arg) {
 
 Vertex2D_t Vertex2D_t_project(Vertex3D_t vector) {
     return  (Vertex2D_t {vector.x, vector.y});
-}
+
 
 Cube_t Cube_t_init(Vertex3D_t position, int size) {
-    int d = size/2;
+    float d = size/2;
     return (Cube_t) {
         d, 
         {
@@ -147,4 +147,9 @@ fillTriangle(imageData, v0, v1, v2) {
 
 void fillTriangle(Vertex2D_t v0, Vertex2D_t v1, Vertex2D_t, v2) {
     
+}
+
+int floor(float value) {
+    re
+  
 }
